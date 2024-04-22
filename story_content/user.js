3f28518a38aebbcdb10cdc2177e5db4d8a3a2848
Manishkumar07091992 <156296553+Manishkumar07091992@users.noreply.github.com>
2024-04-22 16:38:50 +0530

    Generation Game

---
+++ story_content/user.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6SAb1yGv5si":
+        Script1();
+        break;
+      case "5iEejXumGrd":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.close();
+}
+
+function Script2()
+{
+  window.close();
+}
+
